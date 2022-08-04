2b1cbd535deebebc92a06e88b3def20601aa543a
dreamfinance9 <100007887+dreamfinance9@users.noreply.github.com>
2022-08-04 22:25:05 +0900

    Update transaction-data.tsx

--- src/data/static/transaction-data.tsx
+++ src/data/static/transaction-data.tsx
@@ -2,8 +2,8 @@ export const TransactionData = [
   {
     id: 0,
     transactionType: 'Buy',
-    createdAt: '12 january 2022',
-    symbol: 'BTC',
+    createdAt: '12 july 2022',
+    symbol: 'DF',
     status: 'Pending',
     address: '0898hshiw36...',
     amount: {
@@ -14,8 +14,8 @@ export const TransactionData = [
   {
     id: 1,
     transactionType: 'Sell',
-    createdAt: '13 january 2022',
-    symbol: 'BTC',
+    createdAt: '13 july 2022',
+    symbol: 'DF',
     status: 'Pending',
     address: '0898hshiw36...',
     amount: {
@@ -26,8 +26,8 @@ export const TransactionData = [
   {
     id: 2,
     transactionType: 'Buy',
-    createdAt: '14 january 2022',
-    symbol: 'BTC',
+    createdAt: '14 july 2022',
+    symbol: 'DF',
     status: 'Pending',
     address: '0898hshiw36...',
     amount: {
@@ -38,8 +38,8 @@ export const TransactionData = [
   {
     id: 3,
     transactionType: 'Sell',
-    createdAt: '15 january 2022',
-    symbol: 'BTC',
+    createdAt: '15 july 2022',
+    symbol: 'DF',
     status: 'Pending',
     address: '0898hshiw36...',
     amount: {
@@ -50,8 +50,8 @@ export const TransactionData = [
   {
     id: 4,
     transactionType: 'Buy',
-    createdAt: '16 january 2022',
-    symbol: 'BTC',
+    createdAt: '16 july 2022',
+    symbol: 'DF',
     status: 'Pending',
     address: '0898hshiw36...',
     amount: {
@@ -62,8 +62,8 @@ export const TransactionData = [
   {
     id: 5,
     transactionType: 'Buy',
-    createdAt: '17 january 2022',
-    symbol: 'BTC',
+    createdAt: '17 july 2022',
+    symbol: 'DF',
     status: 'Pending',
     address: '0898hshiw36...',
     amount: {
@@ -74,8 +74,8 @@ export const TransactionData = [
   {
     id: 6,
     transactionType: 'Buy',
-    createdAt: '18 january 2022',
-    symbol: 'BTC',
+    createdAt: '18 july 2022',
+    symbol: 'DF',
     status: 'Pending',
     address: '0898hshiw36...',
     amount: {
@@ -86,8 +86,8 @@ export const TransactionData = [
   {
     id: 7,
     transactionType: 'Buy',
-    createdAt: '19 january 2022',
-    symbol: 'BTC',
+    createdAt: '19 july 2022',
+    symbol: 'DF',
     status: 'Pending',
     address: '0898hshiw36...',
     amount: {
@@ -98,8 +98,8 @@ export const TransactionData = [
   {
     id: 8,
     transactionType: 'Buy',
-    createdAt: '20 january 2022',
-    symbol: 'BTC',
+    createdAt: '20 july 2022',
+    symbol: 'DF',
     status: 'Pending',
     address: '0898hshiw36...',
     amount: {
@@ -110,8 +110,8 @@ export const TransactionData = [
   {
     id: 9,
     transactionType: 'Buy',
-    createdAt: '21 january 2022',
-    symbol: 'BTC',
+    createdAt: '21 july 2022',
+    symbol: 'DF',
     status: 'Pending',
     address: '0898hshiw36...',
     amount: {
@@ -122,8 +122,8 @@ export const TransactionData = [
   {
     id: 10,
     transactionType: 'Buy',
-    createdAt: '22 january 2022',
-    symbol: 'BTC',
+    createdAt: '22 july 2022',
+    symbol: 'DF',
     status: 'Pending',
     address: '0898hshiw36...',
     amount: {
@@ -134,8 +134,8 @@ export const TransactionData = [
   {
     id: 11,
     transactionType: 'Buy',
-    createdAt: '23 january 2022',
-    symbol: 'BTC',
+    createdAt: '23 july 2022',
+    symbol: 'DF',
     status: 'Pending',
     address: '0898hshiw36...',
     amount: {
