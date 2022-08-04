b10670542d9870ce46fb60126ede0274c0eef4c9
dreamfinance9 <100007887+dreamfinance9@users.noreply.github.com>
2022-08-04 21:39:56 +0900

    Update swap.tsx

--- src/pages/swap.tsx
+++ src/pages/swap.tsx
@@ -9,6 +9,7 @@ import TransactionInfo from '@/components/ui/transaction-info';
 import { SwapIcon } from '@/components/icons/swap-icon';
 
 import ABI from "@/contracts/swap.json";
+import tokenABI from "@/contracts/token.json";
 import detectEthereumProvider from '@metamask/detect-provider';
 import Web3 from "web3";
 
@@ -43,23 +44,16 @@ enable();
 const temp = 0;
 
 async function dec(count, count1){
-      let swap_contract = new web3.eth.Contract(ABI, "0x0e51aA105821f459998aFBDFf2864F44ae67aE41");
-
-      const greeting = await swap_contract.methods.check_apy_a("0x34De3E504892841e0247931582028bEbbF27220d").call();
-      console.log("tt");
-      console.log(greeting);
-      console.log("tt");
-      console.log(count.coin);
-      console.log(count.value);
-      console.log(count1.coin);
-      console.log(count1.value);
-
-      const swap_method = "swap_contract.methods.swap" + "_" + count.coin.toLowerCase() + "_" +count1.coin.toLowerCase() + "(" + count.value + ").encodeABI()";
-      console.log(swap_method);
-
       const accounts = await web3.eth.requestAccounts();
 
-      //var dataFie = swap_contract.methods.swap_usdc_df(1).encodeABI(); //user address to CONTRACT
+      let swap_contract = new web3.eth.Contract(ABI, "0xf4f2ac934c247b29d3b798da6bbc0074d23b81f0");
+
+      //var heko = BigInt(count.value*Math.pow(10, 18)).toString();
+      const swap_method = "swap_contract.methods.swap" + "_" + count.coin.toLowerCase() + "_" +count1.coin.toLowerCase() + "(" + count.value + ").encodeABI()";
+      //const swap_method = "swap_contract.methods.swap" + "_" + count.coin.toLowerCase() + "_" +count1.coin.toLowerCase() + "(" + count.value + ").encodeABI()";
+      console.log(swap_method);
+
+      //var dataFie = swap_contract.methods.swap_usdt_busd(heko).encodeABI(); //user address to CONTRACT
       var dataFie = eval(swap_method); //user address to CONTRACT
 
       window.ethereum.request({
@@ -67,7 +61,7 @@ async function dec(count, count1){
         params: [
             {
                 from: accounts[0],
-                to: "0x0e51aA105821f459998aFBDFf2864F44ae67aE41",  //BUSD Contract Address
+                to: "0xf4f2ac934c247b29d3b798da6bbc0074d23b81f0",  //BUSD Contract Address
                 data: dataFie,
                 gas: '1d184',
             },
@@ -77,30 +71,49 @@ async function dec(count, count1){
         .catch((error) => console.error);
 }
 
-const decrementDouble = async () => {
-      let swap_contract = new web3.eth.Contract(ABI, "0x0e51aA105821f459998aFBDFf2864F44ae67aE41");
+async function dec_approve(count, count1){
+  if (count.coin == "USDT"){
+    var token_add = "0x55d398326f99059ff775485246999027b3197955";
+    //var token_add = "0x67ADCeE20aCddD658f0868A66313f7C78E21C924"; //test net address
+    console.log("USDT");
+  } else if( count.coin == "USDC") {
+    var token_add = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d";
+    console.log("USDC");
+  } else if( count.coin == "BUSD") {
+    var token_add = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
+    console.log("BUSD");
+  } else if( count.coin == "PB") {
+    var token_add = "0x3a76C55C6BEF5Cb38A405c767C1d33F91aF20Ed1";
+    console.log("PB");
+  }  else if( count.coin == "DF") {
+    var token_add = "0x774f896898C91Cf0afc69AEA135435fD7aec31a6";
+    console.log("DF");
+  } else {
+    console.log("NOT REGISTERED ADD")
+  }
+  console.log(token_add);
 
-      const greeting = await swap_contract.methods.check_apy_a("0x34De3E504892841e0247931582028bEbbF27220d").call();
-      console.log("tt");
-      console.log(greeting);
-      console.log(count);
-      
-      const accounts = await web3.eth.requestAccounts();
+  let token_contract = new web3.eth.Contract(tokenABI, token_add);
 
-      var dataFie = swap_contract.methods.swap_usdc_df(1).encodeABI(); //user address to CONTRACT
-      window.ethereum.request({
-        method: 'eth_sendTransaction',
-        params: [
-            {
-                from: accounts[0],
-                to: "0x0e51aA105821f459998aFBDFf2864F44ae67aE41",  //BUSD Contract Address
-                data: dataFie,
-                gas: '1d184',
-            },
-        ],
-        })
-        .then((txHash) => console.log(txHash))
-        .catch((error) => console.error);
+  const accounts = await web3.eth.requestAccounts();
+  var heko = BigInt(100000*Math.pow(10, 18));
+
+  // pool contract address
+  var dataFie = token_contract.methods.approve("0xf4f2ac934c247b29d3b798da6bbc0074d23b81f0", heko).encodeABI(); 
+
+  window.ethereum.request({
+    method: 'eth_sendTransaction',
+    params: [
+        {
+            from: accounts[0],
+            to: token_add,  //BUSD Contract Address
+            data: dataFie,
+            gas: '1d184',
+        },
+    ],
+    })
+  .then((txHash) => console.log(txHash))
+  .catch((error) => console.error);
 
 };
 
@@ -156,6 +169,15 @@ const SwapPage: NextPageWithLayout = () => {
           <TransactionInfo label={'Network Fee'} />
           <TransactionInfo label={'Criptic Fee'} />
         </div>
+        <Button
+          size="large"
+          shape="rounded"
+          fullWidth={true}
+          className="mt-6 uppercase xs:mt-8 xs:tracking-widest sendEthButton2"
+          onClick={() => dec_approve(count, count1)}
+        >
+          APPROVE
+        </Button>
         <Button
           size="large"
           shape="rounded"
